bfd78eabaa290c5a1fa247f007970d440c3b50d7
Joachim Pileborg <joachimp@axis.com>
2016-05-16 14:01:02 +0200

    Added test lexer program

---
+++ compiler/lexer/test_lexer.cpp
@@ -0,0 +1,20 @@
+//
+// Created by joachimp on 2016-05-16.
+//
+
+#include "buffer.h"
+
+namespace l = compiler::lexer;
+
+namespace
+{
+    void test_lexer_buffer()
+    {
+
+    }
+}
+
+int main()
+{
+    test_lexer_buffer();
+}
