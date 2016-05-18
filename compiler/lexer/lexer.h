b94c24c3bce35693e761563164960d89e0edead9
Joachim Pileborg <joachimp@axis.com>
2016-05-18 09:17:37 +0200

    Refinements of the lexer

--- compiler/lexer/lexer.h
+++ compiler/lexer/lexer.h
@@ -18,9 +18,14 @@ namespace compiler
         class basic_tokenizer
         {
         public:
-            basic_tokenizer()
+            // Not default constructible
+            basic_tokenizer() = delete;
+
+            explicit basic_tokenizer(std::unique_ptr<buffers::basic_buffer<charT>> buffer)
                 : buffers_{}, token_{}
-            {}
+            {
+                buffers_.push(buffer);
+            }
 
             /**
              * \brief Get next token
@@ -29,9 +34,28 @@ namespace compiler
              */
             basic_token<charT> const& get();
 
+            // Disallow copying
+            basic_tokenizer(basic_tokenizer const&) = delete;
+            basic_tokenizer& operator=(basic_tokenizer const&) = delete;
+
+            basic_tokenizer(basic_tokenizer&&) = default;
+            basic_tokenizer& operator=(basic_tokenizer&&) = default;
+
         private:
             std::queue<std::unique_ptr<buffers::basic_buffer<charT>>> buffers_;
             std::unique_ptr<basic_token<charT>> token_;     //!< Last token returned from get()
+
+            template<typename tokenT, typename... argsT>
+            basic_token<charT> const& return_token(argsT... args)
+            {
+                token_ = std::make_unique<tokenT>(std::forward(args...));
+                return *token_;
+            }
+
+            charT next_char()
+            {
+                return buffers_.front().get();
+            }
         };
 
         using tokenizer = basic_tokenizer<char>;
