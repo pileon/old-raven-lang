e463222b857ff73fde8c6930f1b1261f7aaac460
Joachim Pileborg <arrow@pileborg.se>
2016-06-06 18:50:06 +0200

    Working on diagnostics design

--- compiler/diagnostics.h
+++ compiler/diagnostics.h
@@ -1,8 +1,65 @@
-//
-// Created by arrow on 2016-06-06.
-//
-
 #ifndef RAVEN_LANG_DIAGNOSTICS_H
 #define RAVEN_LANG_DIAGNOSTICS_H
+
+#include <exception>
+#include <string>
+
+namespace compiler
+{
+    /// \brief Diagnostic message handling
+    ///
+    class diagnostics
+    {
+    private:
+        template<typename charT>
+        struct basic_outputter
+        {
+            basic_outputter& operator<<(std::basic_string<charT> const&)
+            { return *this; }
+            basic_outputter& operator<<(charT const)
+            { return *this; }
+            basic_outputter& operator<<(short const)
+            { return *this; }
+            basic_outputter& operator<<(int const)
+            { return *this; }
+            basic_outputter& operator<<(long const)
+            { return *this; }
+            basic_outputter& operator<<(long long const)
+            { return *this; }
+            basic_outputter& operator<<(unsigned short const)
+            { return *this; }
+            basic_outputter& operator<<(unsigned int const)
+            { return *this; }
+            basic_outputter& operator<<(unsigned long const)
+            { return *this; }
+            basic_outputter& operator<<(unsigned long long const)
+            { return *this; }
+
+            virtual void pre() = 0;
+            virtual void post() = 0;
+        };
+
+        template<typename charT>
+        struct info_outputter : basic_outputter<charT>
+        {
+            void pre()
+            {
+                // TODO: Output "info" plus file-name, line number, column
+            }
+
+            void post() {}
+        };
+
+    public:
+        template<typename charT>
+        static basic_outputter<charT>& info()
+        {
+            // TODO: Create info_outputter
+            static info_outputter<charT> info;
+            info.pre();
+            return info;
+        }
+    };
+}
 
 #endif //RAVEN_LANG_DIAGNOSTICS_H
---
+++ compiler/test_diagnostics.cpp
@@ -0,0 +1,10 @@
+//
+// Created by arrow on 2016-06-06.
+//
+
+#include "diagnostics.h"
+
+int main()
+{
+    compiler::diagnostics::info<char>() << "foo";
+}
