3bf2cf2dabe98d11ceb213d2ef671ca35e369910
Joachim Pileborg <arrow@pileborg.se>
2016-06-07 02:10:19 +0200

    Actual output being done

--- compiler/diagnostics.h
+++ compiler/diagnostics.h
@@ -3,6 +3,7 @@
 
 #include <exception>
 #include <string>
+#include <iostream>
 
 namespace compiler
 {
@@ -14,51 +15,109 @@ namespace compiler
         template<typename charT>
         struct basic_outputter
         {
-            basic_outputter& operator<<(std::basic_string<charT> const&)
-            { return *this; }
-            basic_outputter& operator<<(charT const)
-            { return *this; }
-            basic_outputter& operator<<(short const)
-            { return *this; }
-            basic_outputter& operator<<(int const)
-            { return *this; }
-            basic_outputter& operator<<(long const)
-            { return *this; }
-            basic_outputter& operator<<(long long const)
-            { return *this; }
-            basic_outputter& operator<<(unsigned short const)
-            { return *this; }
-            basic_outputter& operator<<(unsigned int const)
-            { return *this; }
-            basic_outputter& operator<<(unsigned long const)
-            { return *this; }
-            basic_outputter& operator<<(unsigned long long const)
-            { return *this; }
+            basic_outputter& operator<<(std::basic_string<charT> const& value)
+            {
+                output_ << value;
+                return *this;
+            }
+            basic_outputter& operator<<(charT const value)
+            {
+                output_ << value;
+                return *this;
+            }
+            basic_outputter& operator<<(short const value)
+            {
+                output_ << value;
+                return *this;
+            }
+            basic_outputter& operator<<(int const value)
+            {
+                output_ << value;
+                return *this;
+            }
+            basic_outputter& operator<<(long const value)
+            {
+                output_ << value;
+                return *this;
+            }
+            basic_outputter& operator<<(long long const value)
+            {
+                output_ << value;
+                return *this;
+            }
+            basic_outputter& operator<<(unsigned short const value)
+            {
+                output_ << value;
+                return *this;
+            }
+            basic_outputter& operator<<(unsigned int const value)
+            {
+                output_ << value;
+                return *this;
+            }
+            basic_outputter& operator<<(unsigned long const value)
+            {
+                output_ << value;
+                return *this;
+            }
+            basic_outputter& operator<<(unsigned long long const value)
+            {
+                output_ << value;
+                return *this;
+            }
 
-            virtual void pre() = 0;
-            virtual void post() = 0;
+            virtual basic_outputter& pre() = 0;
+            virtual basic_outputter& post() = 0;
+
+            basic_outputter() = delete;
+            basic_outputter(basic_outputter const&) = default;
+            basic_outputter(basic_outputter&&) = default;
+
+            basic_outputter(std::basic_ostream<charT>& os)
+                : output_(os)
+            {}
+
+            virtual ~basic_outputter() {}
+
+            std::basic_ostream<charT>& output_;
         };
+        // TODO: Specialization of basic_outputter for handling char and wchar_t respectively?
+        // TODO: Then the class can handle both char and wchar_t overloads of the << operator,
+        // TODO: converting as needed
 
         template<typename charT>
         struct info_outputter : basic_outputter<charT>
         {
-            void pre()
+            info_outputter(std::basic_ostream<charT>& os)
+                : basic_outputter<charT>(os)
+            {}
+
+            basic_outputter<charT>& pre()
             {
+                return *this << "info:";
                 // TODO: Output "info" plus file-name, line number, column
+                // TODO: How to get file-name, line number or column?
+                return *this;
             }
 
-            void post() {}
+            basic_outputter<charT>& post()
+            {
+                return *this;
+            }
         };
 
     public:
+        /// \brief To output informational messages
+        /// \return An object that outputs informational messages
         template<typename charT>
-        static basic_outputter<charT>& info()
+        static basic_outputter<charT> basic_info()
         {
-            // TODO: Create info_outputter
-            static info_outputter<charT> info;
-            info.pre();
-            return info;
+            return info_outputter<charT>{std::clog}.pre();
         }
+
+        static constexpr auto info = basic_info<char>;
+
+    private:
     };
 }
 
--- compiler/test_diagnostics.cpp
+++ compiler/test_diagnostics.cpp
@@ -6,5 +6,5 @@
 
 int main()
 {
-    compiler::diagnostics::info<char>() << "foo";
+    compiler::diagnostics::info() << "foo";
 }
